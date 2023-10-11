7bfe20d4df59d6e720d4f7efd5fa41e44e984ac1
Iago Novaes <iagonovaes19@gmail.com>
2023-10-10 22:14:32 -0300

    modification

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import styles from "./Header.module.css"
-import igniteLogo from "../asset/ignite-logo.svg"
+// import igniteLogo from "../asset/ignite-logo.svg"
 
 export function Header(){
   return(
   <header className={styles.header}>
-    <img src={igniteLogo} alt="LogoTipo Ignite" />
+    {/* <img src={igniteLogo} /> */}
   </header>
     
   )
